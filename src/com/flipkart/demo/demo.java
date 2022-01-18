ce7046055b884e21aac5aa4d08ec43050e34c652
savan60 <51092808+savan60@users.noreply.github.com>
2022-01-18 11:10:52 +0530

    this is the first commit

---
+++ src/com/flipkart/demo/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author SAVAN
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the test project");
+	}
+
+}
